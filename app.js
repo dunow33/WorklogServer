3b64476e13c87b4d9c19eb36dda7ef5980264000
Brian Donnoe <dunow33@hotmail.com>
2017-09-08 15:50:21 -0400

    heroku fixes

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use('/api/test', function(req, res){
 	res.send("Hello World!!");
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
 	console.log('App is listening on 3000.')
 });
