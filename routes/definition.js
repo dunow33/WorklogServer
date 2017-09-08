5450c2b864d8547a2760c43a722da1d3ab329ce9
Brian Donnoe <dunow33@hotmail.com>
2017-09-08 13:36:38 -0400

    initial server commit

---
+++ routes/definition.js
@@ -0,0 +1,52 @@
+var router = require('express').Router();
+var sequelize = require('../db');
+var User = sequelize.import('../models/user');
+var Definition = sequelize.import('../models/definition');
+
+router.post('/', function(req, res) {
+	//variables
+    var description = req.body.definition.desc;
+    var logType= req.body.definition.type;
+    var owner = req.user.id;
+
+	
+	//objects must match the model
+	Definition.create({ 
+		   	description: description,
+		   	logType: logType,
+		   	owner: owner
+		}).then(
+			function createSuccess(definition) {
+			//send a response as json
+	   		res.json({
+	   			definition: definition
+	   		});
+		   	}, 
+		   function createError(err) {
+		       res.send(500, err.message);
+		});
+});
+
+router.get('/', function(req, res) {
+	//user variable
+	var userid = req.user.id;
+	
+	//findAll by owner method
+	Definition.findAll({
+			where: { owner: userid }
+		})
+		.then(
+			//success
+			function findAllSuccess(data) {
+				// console.log(data);
+				res.json(data);
+			},
+			//failure
+			function findAllError(err) {
+				res.send(500, err.message);
+			}
+		);
+});
+
+
+module.exports = router;
